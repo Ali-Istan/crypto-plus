b4612d44e466876b46a5547d3e1a8709c9f14fad
Ali Istan <ali.istan@outlook.com>
2024-07-31 19:58:35 +0330

    update

--- lib/Screen/coin_list_screen.dart
+++ lib/Screen/coin_list_screen.dart
@@ -3,6 +3,7 @@ import 'package:crypto_plus/Model/crypto.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CoinListScreen extends StatefulWidget {
   final List<CryptoModel> cryptoModel;
@@ -15,6 +16,7 @@ class CoinListScreen extends StatefulWidget {
 
 class _CoinListScreenState extends State<CoinListScreen> {
   late List<CryptoModel> cryptolist;
+  bool isSearchLoadingVisible = false;
 
   @override
   void initState() {
@@ -36,21 +38,83 @@ class _CoinListScreenState extends State<CoinListScreen> {
       ),
       backgroundColor: MyColors.black,
       body: SafeArea(
-          child: RefreshIndicator(
-        onRefresh: () async {
-          List<CryptoModel> refresh = await _getData() ;
-          setState(() {
-            cryptolist = refresh;
-          });
-        },
-        child: ListView.builder(
-          itemCount: cryptolist.length,
-          itemBuilder: (context, index) {
-              return  _getList(cryptolist[index]);
-          },
-        ),
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Directionality(
+              textDirection: TextDirection.rtl,
+              child: TextField(
+                onChanged: (value) {
+                  _filterList(value);
+                },
+                decoration: InputDecoration(
+                    hintText: 'اسم رمزارز معتبر خودتونو سرچ کنید',
+                    hintStyle:
+                        const TextStyle(fontFamily: 'mr', color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                      borderSide:
+                          const BorderSide(width: 0, style: BorderStyle.none),
+                    ),
+                    filled: true,
+                    fillColor: Colors.green),
+              ),
+            ),
+          ),
+          Visibility(
+            visible: isSearchLoadingVisible,
+            child: const Text(
+              '...در حال اپدیت اطلاعات رمز ارزها',
+              style: TextStyle(color: Colors.green, fontFamily: 'mr'),
+            ),
+          ),
+          Expanded(
+            child: RefreshIndicator(
+              backgroundColor: Colors.white,
+              color: Colors.green,
+              onRefresh: () async {
+                List<CryptoModel> refresh = await _getData();
+                setState(() {
+                  cryptolist = refresh;
+                });
+              },
+              child: ListView.builder(
+                itemCount: cryptolist.length,
+                itemBuilder: (context, index) {
+                  return _getList(cryptolist[index]);
+                },
+              ),
+            ),
+          ),
+        ],
       )),
     );
+  }
+
+  Future<void> _filterList(String search) async {
+    List<CryptoModel> cryptoResultList = [];
+
+    if (search.isEmpty) {
+      setState(() {
+        isSearchLoadingVisible = true;
+      });
+      var result = await _getData();
+      setState(() {
+        cryptolist = result;
+        isSearchLoadingVisible = false;
+      });
+      return;
+    }
+
+    cryptolist
+        .where(
+          (element) => element.name.toLowerCase().contains(search),
+        )
+        .toList();
+    setState(() {
+      cryptolist = cryptoResultList;
+    });
   }
 
   Widget _getIconChangePercent(double percentChanged) {
@@ -90,24 +154,22 @@ class _CoinListScreenState extends State<CoinListScreen> {
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Text(
-                  double.parse(crypto.priceUSD)
-                      .toStringAsFixed(2),
+                  double.parse(crypto.priceUSD).toStringAsFixed(2),
                   style: TextStyle(color: Colors.grey),
                 ),
                 Text(
-                  double.parse(crypto.changePercet24hr)
-                      .toStringAsFixed(2),
+                  double.parse(crypto.changePercet24hr).toStringAsFixed(2),
                   style: TextStyle(
-                    color: _getColorChangeText(double.parse(
-                        crypto.changePercet24hr)),
+                    color: _getColorChangeText(
+                        double.parse(crypto.changePercet24hr)),
                   ),
                 ),
               ],
             ),
             SizedBox(
               width: 30,
-              child: _getIconChangePercent(
-                  double.parse(crypto.changePercet24hr)),
+              child:
+                  _getIconChangePercent(double.parse(crypto.changePercet24hr)),
             ),
           ],
         ),
